a4bf1c4a08211c6f4412f99fcc8ff69dfe9c7b8f
bambadiagne <diagne.bambahmadou@gmail.com>
2024-10-22 16:09:39 +0000

    feat: configure cors and fix message type

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.use(cookieParser());
   await app.listen(3000);
 }
